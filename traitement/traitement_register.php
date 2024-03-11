6356bfc3c751b1220025f4ed1ef863a3ad1f9c53
mehdi <mehdi@mehdis-MacBook-Pro.local>
2024-03-11 12:59:50 +0100

    mehdi: ajout de traitement connexion

--- traitement/traitement_register.php
+++ traitement/traitement_register.php
@@ -2,19 +2,19 @@
 
 session_start();
 
-if (isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['id']) && isset($_POST['password']))
+if (isset($_POST['type']) && isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['id']) && isset($_POST['password']))
 {
     $file_name = 'users.csv';
     $file = fopen($file_name, 'a');
 
     if (filesize($file_name) == 0)
     {
-        fputcsv($file, ['lastname', 'firstname', 'id', 'password']);
+        fputcsv($file, ['type', 'lastname', 'firstname', 'id', 'password']);
     }
 
     $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    fputcsv($file, [$_POST['lastname'], $_POST['firstname'], $_POST['id'], $password_hash]);
+    fputcsv($file, [$_POST['type'], $_POST['lastname'], $_POST['firstname'], $_POST['id'], $password_hash]);
     fclose($file);
     $_SESSION['id'] = $_POST['id'];
     header('location: ../accueil/header.php');
