7c79e9053a3fc60ccb38880e6d0aa4c9bac58e44
amine-ipssi-25 <a.mahi@ecole-ipssi.net>
2024-03-11 11:09:15 +0100

    V0 de la page d'acceuil

---
+++ accueil/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizzeo</title>
+    <link rel="stylesheet" href="header.css">
+    <script src="https://kit.fontawesome.com/3b9e7859ca.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class='logo'>
+                <img src="./assets/quizzeo.png" alt="logo"/> 
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>My Quizz</li>
+                <li>Create</li>
+            </ul>
+        </nav>
+        <div class = 'login'>
+            <a href="../connexion/connexion.php"><i class="fa-solid fa-user" style="color: #9a79fb;"></i></a>
+            <a href="../connexion/connexion.php"><h2>Connexion</h2></a>
+        </div>
+    </header>
+</body>
+</html>
