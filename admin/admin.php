d85185faa587e2bb47043b28819dec1dc7236996
mehdi <mehdi@mehdis-MacBook-Pro.local>
2024-03-12 11:49:03 +0100

    mehdi: ajout de la page admin

--- admin/admin.php
+++ admin/admin.php
@@ -11,5 +11,37 @@ include('../accueil/header.php');
     </head>
     <body>
         <h1>Admin page</h1>
+        <ul>
+            <?php
+            $file_name = '../traitement/users.csv';
+            $file_open = fopen($file_name,'r');
+
+            $Row1 = true;
+            while (($line = fgetcsv($file_open)) !== FALSE)
+            {
+                echo "<li>";
+
+                foreach ($line as $cell)
+                {
+                    echo "<span>$cell</span>";
+                }
+
+                echo "</li>";
+
+                if ($Row1)
+                {
+                    echo "<li><strong>Row 0:</strong>";
+                }
+
+                foreach ($line as $cell)
+                {
+                    echo "<span>$cell</span>";
+                }
+                echo "</li>";
+                $Row1 = false;
+            }
+            fclose($file_open);
+            ?>
+        </ul>
     </body>
 </html>
