e4a3d22bddafd63d0f654506dc21dc06086140f6
Solal Ribezzo <solal.ribezzo2003@gmail.com>
2024-03-13 16:49:16 +0100

    solal: debut page d'accueil

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -8,6 +8,7 @@ include('../accueil/header.php');
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link  rel="stylesheet" href="../accueil/accueil.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
 </head>
