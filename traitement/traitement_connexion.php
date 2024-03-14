0f662dc4d8e040df08bc6fd71eb688db9be58290
mehdi <m.msakni@ecole-ipssi.net>
2024-03-14 13:17:17 +0100

    renforcement de la securite

--- traitement/traitement_connexion.php
+++ traitement/traitement_connexion.php
@@ -9,37 +9,44 @@ if (isset($_POST['id']) && isset($_POST['password'])) {
     if ($file) {
         while (($line = fgetcsv($file)) !== false) {
             if ($line[3] === $_POST['id']) {
-                if (password_verify($_POST['password'], $line[4])) {
+                if (password_verify($_POST['password'], $line[4]))
+                {
+                    if ($line[5] == '1')
+                    {
+                        if (($line[0] == 'User'))
+                        {
+                            $_SESSION['id'] = $_POST['id'];
+                            fclose($file);
+                            header('location: ../user/user.php');
+                            exit();
+                        }
+                        if (($line[0] == 'School'))
+                        {
+                            $_SESSION['id'] = $_POST['id'];
+                            fclose($file);
+                            header('location: ../school/school.php');
+                            exit();
+                        }
+                        if (($line[0] == 'Company'))
+                        {
+                            $_SESSION['id'] = $_POST['id'];
+                            fclose($file);
+                            header('location: ../company/company.php');
+                            exit();
+                        }
 
-                    if (($line[0] == 'User'))
-                    {
-                        $_SESSION['id'] = $_POST['id'];
-                        fclose($file);
-                        header('location: ../user/user.php');
-                        exit();
+                        // admin page
+                        if (($line[0] == 'Admin')) {
+                            $_SESSION['id'] = $_POST['id'];
+                            $_SESSION['admin'] = true;
+                            fclose($file);
+                            header('location: ../admin/admin.php');
+                            exit();
+                        }
                     }
-                    if (($line[0] == 'School'))
+                    else 
                     {
-                        $_SESSION['id'] = $_POST['id'];
-                        fclose($file);
-                        header('location: ../school/school.php');
-                        exit();
-                    }
-                    if (($line[0] == 'Company'))
-                    {
-                        $_SESSION['id'] = $_POST['id'];
-                        fclose($file);
-                        header('location: ../company/company.php');
-                        exit();
-                    }
-
-                    // admin page
-                    if (($line[0] == 'Admin')) {
-                        $_SESSION['id'] = $_POST['id'];
-                        $_SESSION['admin'] = true;
-                        fclose($file);
-                        header('location: ../admin/admin.php');
-                        exit();
+                        $error = "L'utilisateur a été desactiver";
                     }
                 }
                 else
