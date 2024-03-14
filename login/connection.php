d04618f4705df13228e61730f3a2b538a037ff1a
Solal Ribezzo <solal.ribezzo2003@gmail.com>
2024-03-14 14:12:33 +0100

    solal: captcha

--- login/connection.php
+++ login/connection.php
@@ -14,14 +14,31 @@ if(empty($_SESSION['id'])){
         </head>
 
         <body>
+        <script>
+        function verifierCaptcha() {
+        // Récupérer la réponse du captcha
+        var response = grecaptcha.getResponse();
+
+        // Vérifier si la réponse n'est pas vide
+        if (response.length == 0) {
+            // Le captcha n'a pas été fait
+            alert("Veuillez remplir le captcha.");
+            return false;
+        } else {
+        // Le captcha a été fait, continuer avec la soumission du formulaire
+            return true;
+        }
+    }
+</script>
+
             <div class="container">
                 <h2>Connexion</h2>
-                <form action="../traitement/traitement_connexion.php" method="post">
+                <form action="../traitement/traitement_connexion.php" method="post" onsubmit="return verifierCaptcha()">
                     <label for="id">Identifiant :</label>
                     <input type="text" id="id" name="id" required>
                     <label for="password">Mot de passe :</label>
                     <input type="password" id="password" name="password" required>
-                    <div class="g-recaptcha" data-sitekey="6LcWdpYpAAAAADPRbtC1iHBXHMHE9XI56grmpPHz"></div>
+                    <div class="g-recaptcha" data-sitekey="6Le3opgpAAAAAPqnC4hdvBDDCnKvpcMSa9siPbPX"></div>
                     <input type="submit" value="Se connectez">
                 </form>
                 <p>You have not an account ? <a href="register.php"> Register</a></p>
--- traitement/traitement_connexion.php
+++ traitement/traitement_connexion.php
@@ -2,6 +2,9 @@
 session_start();
 $error;
 
+$secretKey = "6Le3opgpAAAAAMARXyq12uDgBjspwqRGypJlHc38";
+
+
 if (isset($_POST['id']) && isset($_POST['password'])) {
     $file_name = 'users.csv';
     $file = fopen($file_name, 'r');
@@ -59,4 +62,53 @@ if (isset($_POST['id']) && isset($_POST['password'])) {
         $error = "Erreur lors de l'ouverture du fichier.";
     }
     echo $error;
+
+    if(isset($_POST['g-recaptcha-response'])) {
+        // Vérifiez la réponse du captcha avec Google
+        $captchaResponse = $_POST['g-recaptcha-response'];
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = array(
+            'secret' => $secretKey,
+            'response' => $captchaResponse
+        );
+    
+        $options = array(
+            'http' => array (
+                'method' => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+    
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+        $responseKeys = json_decode($response, true);
+    }
+
 }
+
+// Votre clé secrète reCAPTCHA
+//$secretKey = "6Le3opgpAAAAAMARXyq12uDgBjspwqRGypJlHc38";
+
+// Vérifiez si le captcha a été soumis
+    if(isset($_POST['g-recaptcha-response'])) {
+    // Vérifiez la réponse du captcha avec Google
+    $captchaResponse = $_POST['g-recaptcha-response'];
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = array(
+        'secret' => $secretKey,
+        'response' => $captchaResponse
+    );
+
+    $options = array(
+        'http' => array (
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+
+    $context = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+    $responseKeys = json_decode($response, true);
+
+}
+?>
