de0f15e630cb335fc33bf39a598e89ba4d18ea32
mehdi <m.msakni@ecole-ipssi.net>
2024-03-15 10:53:44 +0100

    mehdi: ajout de l'email a l'inscription et ajout d'une securite lors de l'ajout de l'email

--- traitement/traitement_register.php
+++ traitement/traitement_register.php
@@ -2,15 +2,20 @@
 
 session_start();
 
-if (isset($_POST['type']) && isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['id']) && isset($_POST['password']))
+if (isset($_POST['type']) && isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['id']) && isset($_POST['password']) && isset($_POST['Email']))
 {
-    $file_name = 'users.csv';
-    $file = fopen($file_name, 'a');
-
-    $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-    fputcsv($file, [$_POST['type'], $_POST['lastname'], $_POST['firstname'], $_POST['id'], $password_hash, '1']);
-    fclose($file);
-    $_SESSION['id'] = $_POST['id'];
-    header('location: ../accueil/accueil.php');
+    $expression = '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/';
+    $email = $_POST['Email'];
+    if (preg_match($expression, $email))
+    {
+        $file_name = 'users.csv';
+        $file = fopen($file_name, 'a');
+    
+        $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    
+        fputcsv($file, [$_POST['type'], $_POST['lastname'], $_POST['firstname'], $_POST['id'], $password_hash, '1', $_POST['Email']]);
+        fclose($file);
+        $_SESSION['id'] = $_POST['id'];
+        header('location: ../accueil/accueil.php');
+    }
 }
--- user/user.php
+++ user/user.php
@@ -1,15 +1,28 @@
 <?php
 include '../accueil/header.php';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>User page</h1>
-    </body>
-</html>
+if ($_SESSION['id'] == 'User')
+{?>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>User</title>
+        </head>
+        <body>
+            <h1>User page</h1>
+        </body>
+    </html><?php
+}
+else
+{
+    header('location: ../accueil/accueil.php');
+}
+
+// faire ceci:
+// Rôle Utilisateur
+// Le rôle utilisateur permet simplement de répondre aux questionnaires. La page de base lors de la connexion est un dashboard qui indique simplement les questionnaires auxquels il a déjà répondu.
+// Il doit également avoir à sa disposition une page de gestion de profil lui permettant de modifier ses informations personnelles (nom, prénom, adresse mail, mot de passe, ...)
+
+// L'accès aux quiz se fait sur la fourniture d'un lien simple d'accès direct au quiz.
