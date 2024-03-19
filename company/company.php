1d3985e2ef0d19922ab76590fb0e725e963e47bd
Solal Ribezzo <solal.ribezzo2003@gmail.com>
2024-03-19 11:10:29 +0100

    solal css

--- company/company.php
+++ company/company.php
@@ -12,5 +12,9 @@ include('../accueil/header.php');
     </head>
     <body>
         <h1>Company page</h1>
-    </body>
+        <div class="text-content">
+        <h3>Merci de votre confiance ! Vous avez réussi avec succès a vous connecter en temps que company, 
+            vous pouvez maintenant créer des quiz pour votre entreprise et les partagés au sein de celle-ci.</h3>
+            </div> 
+    </body>  
 </html>
--- school/school.php
+++ school/school.php
@@ -12,5 +12,9 @@ include('../accueil/header.php');
     </head>
     <body>
         <h1>School page</h1>
+        <div class="text-content">
+        <h3>Merci de votre confiance ! Vous avez réussi avec succès a vous connecter en temps que School, 
+            vous pouvez maintenant créer des quiz pour votre classe et les partagés aux différents élèves de vos classes.</h3>
+            </div>
     </body>
 </html>
