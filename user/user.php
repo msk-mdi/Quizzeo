b1bcc55714bf9c4aec273d5fb1407037967e2d7d
Solal Ribezzo <solal.ribezzo2003@gmail.com>
2024-03-21 22:42:10 +0100

    solal amelioration css accueil

--- user/user.php
+++ user/user.php
@@ -6,6 +6,7 @@ include '../accueil/header.php';
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link  rel="stylesheet" href="../user/user.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
